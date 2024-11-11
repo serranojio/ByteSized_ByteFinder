ee8c00e161d8b204123c935f9d557147e1c30aef
serranojio <qjaserrano@tip.edu.ph>
2024-11-11 14:32:49 +0800

    modified:   flask_ip_app/ip_app.py

--- flask_ip_app/ip_app.py
+++ flask_ip_app/ip_app.py
@@ -2,34 +2,33 @@ from flask import Flask, render_template, request, jsonify
 from requests import get
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-def get_ip_details(ip=None, version="v4"):
-    """
-    Getting the IP details function. This function uses the ipapi.co API
-    to obtain IP address details for:
-        a) user input
-        b) user current public IP
-    """
-    url = f'https://ipapi.co/{ip}/json/' if ip else f'https://ipapi.co/{version}/json/'
-    response = get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Error retrieving IP details for {version.upper()}.")
-        return None
+# def get_ip_details(ip=None, version="v4"):
+#     """
+#     Getting the IP details function. This function uses the ipapi.co API
+#     to obtain IP address details for:
+#         a) user input
+#         b) user current public IP
+#     """
+#     url = f'https://ipapi.co/{ip}/json/' if ip else f'https://ipapi.co/{version}/json/'
+#     response = get(url)
+#     if response.status_code == 200:
+#         return response.json()
+#     else:
+#         print(f"Error retrieving IP details for {version.upper()}.")
+#         return None
     
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/get_ip_details', methods=['POST'])
-def fetch_ip_details():
-    data = request.json
-    ip = data.get("ip", None).strip() if data.get("ip") else None 
-    version = data.get("version", "v4")
-    ip_details = get_ip_details(ip, version)
-    return jsonify(ip_details)
+# @app.route('/get_ip_details', methods=['POST'])
+# def fetch_ip_details():
+#     data = request.json
+#     ip = data.get("ip", None).strip() if data.get("ip") else None 
+#     version = data.get("version", "v4")
+#     ip_details = get_ip_details(ip, version)
+#     return jsonify(ip_details)
 
 if __name__ == '__main__':
     app.run(debug=True)
